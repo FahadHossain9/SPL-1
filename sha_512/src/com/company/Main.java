5b98628c83db7f5877526d3ee1710c81530b016c
FahadHossain9 <48786675+FahadHossain9@users.noreply.github.com>
2020-02-19 03:45:26 -0800

    Add files via upload

--- sha_512/src/com/company/Main.java
+++ sha_512/src/com/company/Main.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
-  //  public  static int k=0;
-
+//public void padding(int []);
+  private static   int [] array_storing_bit = new int [10000];
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter your text: ");
@@ -15,7 +15,7 @@ public class Main {
         int i=0,y=0;
 
          int[] bitform = new int[8];
-         int [] array_storing_bit = new int [10000];
+
 
          while(i<str.length()){
             x[i] = (int)chars[i] ;
@@ -38,10 +38,39 @@ public class Main {
             }
             i++;
         }
+        padding(array_storing_bit);
         for(int l=0;l<y;l++) {
             System.out.print(array_storing_bit[l]+" ");
         }
         in.close();
     }
-    void padding()
+    public static void padding(int arr[]){
+        int i,last_initialization;
+       int array_Length =  arr.length;
+       int running_bit_position = 0,m;
+       int number_of_block ;
+       if( array_Length%1024==0){
+          number_of_block = array_Length/1024;
+       }else {
+           number_of_block = array_Length/1024 + 1;
+       }
+       int[][] blockwise_bits = new int [number_of_block][1024];
+       for( m=0;m<number_of_block;m++) {
+           for ( i = 0; i < 1024 && running_bit_position<array_Length; i++) {
+                 blockwise_bits[m][i]=arr[running_bit_position];
+                 running_bit_position++;
+           }
+       }
+
+       last_initialization = array_Length-m*1024-i;
+       
+       for(;last_initialization<1024;last_initialization++){
+           blockwise_bits[m][last_initialization]=0;
+       }
+      for(int q=0;q<1024;q++){
+          System.out.print(blockwise_bits[m][q]+" ");
+      }
+    }
+
+
 }
