5722fa1c414f19fabce818589745798942c63d05
FahadHossain9 <48786675+FahadHossain9@users.noreply.github.com>
2020-02-26 00:55:57 -0800

    Add files via upload

--- sha_512/src/com/company/Main.java
+++ sha_512/src/com/company/Main.java
@@ -1,55 +1,57 @@
 package com.company;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
 
-  private static   int [] array_storing_bit = new int [10000];
+  private static   int [] arrayStoringBit = new int [10000];
     public static void main(String[] args) {
+        //taking input text as string
         Scanner in = new Scanner(System.in);
         System.out.print("Enter your text: ");
         String str = in.next();
+        //making the string to character array
         char [] chars = str.toCharArray();
-        int [] x = new int[100000];
-        int i=0,y=0;
+        int [] arrayStoringAsciiValues = new int[100000];
+        int iterator=0,y=0;
+        int[] bitform = new int[8];
 
-         int[] bitform = new int[8];
-
-        System.out.println("length of the input is "+str.length());
-         while(i<str.length()){
-            x[i] = (int)chars[i] ;
-            int num = x[i];
+         while(iterator<str.length()){
+            arrayStoringAsciiValues[iterator] = (int)chars[iterator] ;
+            int num = arrayStoringAsciiValues[iterator];
+            //storing the reverse binary form
             int j=0;
             while(num>0){
                 bitform[j]=num%2;
                 num=num/2;
                 j++;
             }
+            //adding 0 to  make each charcter binary form to 8 bit
             while(j<=7){
                 bitform[j]=0;
                 j++;
             }
+            //reverse the reverse form to store actual binary form
             j=7;
             while(j>=0){
-               array_storing_bit[y]= bitform[j];
+               arrayStoringBit[y]= bitform[j];
                y++;
                j--;
             }
-            i++;
+            iterator++;
         }
-        padding(array_storing_bit,y);
+        pad(arrayStoringBit,y);
 
         in.close();
     }
-    public static void padding(int arr[],int array_Length){
+    public static void pad(int arr[], int arrayLength){
         int i,last_initialization;
        int running_bit_position = 0,m;
        int number_of_block ;
-       if( array_Length%1024==0){
-          number_of_block = array_Length/1024;
+       if( arrayLength%1024==0){
+          number_of_block = arrayLength/1024;
        }else {
-           number_of_block = array_Length/1024 + 1;
+           number_of_block = arrayLength/1024 + 1;
        }
         System.out.println("number of blocks are " + number_of_block);
        int[][] blockwise_bits = new int [number_of_block][1024];
@@ -62,7 +64,7 @@ public class Main {
        }
         System.out.println(running_bit_position);
         i=0;
-         while(running_bit_position<array_Length){
+         while(running_bit_position<arrayLength){
              blockwise_bits[m][i] =arr[running_bit_position];
              running_bit_position++;
              i++;
@@ -77,7 +79,7 @@ public class Main {
           System.out.print(blockwise_bits[u][q]+" ");
 
       }
-            System.out.println("no "+ u+" has been completed with padding");
+            System.out.println("no "+ u +" has been completed with padding");
       }
 
     }
