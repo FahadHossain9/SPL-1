f46ddb593b43c10cfbfb7a61284eb9accedec004
FahadHossain9 <48786675+FahadHossain9@users.noreply.github.com>
2020-02-26 04:27:52 -0800

    Add files via upload

--- sha_512/src/com/company/Main.java
+++ sha_512/src/com/company/Main.java
@@ -3,50 +3,65 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
-  private static   int [] arrayStoringBit = new int [10000];
+    private static int[][] blockwiseBits;
+    private static   int [] arrayStoringBit = new int [10000];
+    public static int [][] wordsFromBlocks;
     public static void main(String[] args) {
+
+
         //taking input text as string
         Scanner in = new Scanner(System.in);
         System.out.print("Enter your text: ");
         String str = in.next();
+
+
         //making the string to character array
         char [] chars = str.toCharArray();
         int [] arrayStoringAsciiValues = new int[100000];
-        int iterator=0,y=0;
+        int iterator=0,iterator2=0;
         int[] bitform = new int[8];
+
 
          while(iterator<str.length()){
             arrayStoringAsciiValues[iterator] = (int)chars[iterator] ;
-            int num = arrayStoringAsciiValues[iterator];
+            int asciiValueOfACharacter = arrayStoringAsciiValues[iterator];
             //storing the reverse binary form
-            int j=0;
-            while(num>0){
-                bitform[j]=num%2;
-                num=num/2;
-                j++;
+            int iteratorFromReverse=0;
+            while(asciiValueOfACharacter>0){
+                bitform[iteratorFromReverse]=asciiValueOfACharacter%2;
+                asciiValueOfACharacter=asciiValueOfACharacter/2;
+                iteratorFromReverse++;
             }
+
             //adding 0 to  make each charcter binary form to 8 bit
-            while(j<=7){
-                bitform[j]=0;
-                j++;
+            while(iteratorFromReverse<=7){
+                bitform[iteratorFromReverse]=0;
+                iteratorFromReverse++;
             }
+
             //reverse the reverse form to store actual binary form
-            j=7;
-            while(j>=0){
-               arrayStoringBit[y]= bitform[j];
-               y++;
-               j--;
+            iteratorFromReverse=7;
+            while(iteratorFromReverse>=0){
+               arrayStoringBit[iterator2]= bitform[iteratorFromReverse];
+               iterator2++;
+               iteratorFromReverse--;
             }
             iterator++;
         }
-        pad(arrayStoringBit,y);
+
+         //passing the bits to pad function to make th array multiple of 1024 bits
+        blockwiseBits = pad(arrayStoringBit,iterator2);
+        wordGenerator(blockwiseBits);
 
         in.close();
     }
-    public static void pad(int arr[], int arrayLength){
-        int i,last_initialization;
-       int running_bit_position = 0,m;
+
+
+
+
+    public static int[][] pad(int actualBinaryFormStored[], int arrayLength){
+        int iteratorForBlocksWithin0To1024,last_initialization;
+       int runningBitPosition = 0,currentBlock;
        int number_of_block ;
        if( arrayLength%1024==0){
           number_of_block = arrayLength/1024;
@@ -54,35 +69,43 @@ public class Main {
            number_of_block = arrayLength/1024 + 1;
        }
         System.out.println("number of blocks are " + number_of_block);
-       int[][] blockwise_bits = new int [number_of_block][1024];
-       for( m=0;m<number_of_block-1;m++) {
-           for ( i = 0; i < 1024 ; i++) {
-                 blockwise_bits[m][i]=arr[running_bit_position];
-                 running_bit_position++;
+         blockwiseBits = new int [number_of_block][1024];
+       //initialize the block from plain text which is already 1024 long & do not need padding
+       for( currentBlock=0;currentBlock<number_of_block-1;currentBlock++) {
+           for ( iteratorForBlocksWithin0To1024 = 0; iteratorForBlocksWithin0To1024 < 1024 ; iteratorForBlocksWithin0To1024++) {
+                 blockwiseBits[currentBlock][iteratorForBlocksWithin0To1024]=actualBinaryFormStored[runningBitPosition];
+                 runningBitPosition++;
            }
 
        }
-        System.out.println(running_bit_position);
-        i=0;
-         while(running_bit_position<arrayLength){
-             blockwise_bits[m][i] =arr[running_bit_position];
-             running_bit_position++;
-             i++;
+       //last block which is short of 1024 bit & need padding
+        iteratorForBlocksWithin0To1024=0;
+         while(runningBitPosition<arrayLength){
+             blockwiseBits[currentBlock][iteratorForBlocksWithin0To1024] =actualBinaryFormStored[runningBitPosition];
+             runningBitPosition++;
+             iteratorForBlocksWithin0To1024++;
          }
-         blockwise_bits[m][i]=1;
-         i++;
+         blockwiseBits[currentBlock][iteratorForBlocksWithin0To1024]=1;
+         iteratorForBlocksWithin0To1024++;
          //padding to make it 1024
-      for(;i<1024;i++){
-          blockwise_bits[m][i]=0;
-      }for(int u=0;u<=m;u++){
-      for(int q=0;q<1024;q++){
-          System.out.print(blockwise_bits[u][q]+" ");
+      for(;iteratorForBlocksWithin0To1024<1024;iteratorForBlocksWithin0To1024++){
+          blockwiseBits[currentBlock][iteratorForBlocksWithin0To1024]=0;
+      }
+        for(int u=0;u<=currentBlock;u++){
+            for(int q=0;q<1024;q++){
+                System.out.print(blockwiseBits[u][q]+" ");
 
-      }
+            }
             System.out.println("no "+ u +" has been completed with padding");
-      }
+        }
+
+      return blockwiseBits;
 
     }
+ public static void  wordGenerator(int [][]blockwiseBits){
+
+ }
+
 
 
 }
