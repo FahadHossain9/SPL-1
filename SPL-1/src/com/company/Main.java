d7f1949ef8d8518abc31cf7b19bdda691284ca58
FahadHossain9 <48786675+FahadHossain9@users.noreply.github.com>
2021-08-21 18:05:09 +0600

    Add files via upload

--- SPL-1/src/com/company/Main.java
+++ SPL-1/src/com/company/Main.java
@@ -5,11 +5,20 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-       Scanner sc = new Scanner(System.in);
-       String  input = sc.nextLine();
-       String hashMessage = sha512(input.getBytes());
+    public static void main(String[] args) throws IOException{
+        //If the input is not in correct format
+        // if(args.length<1)
+        Scanner sc = new Scanner(System.in);
+        String  input = sc.nextLine();
+        System.out.println("Which level of encryption do you want?\n [Press 1] Sha512\n [Press 2] Sha256\n");
+        int choice;
+        choice=sc.nextInt();
+        if(choice==1) {
+            String hashMessage = sha512(input.getBytes());
+        }
+        else if(choice==2) {
+            String hashMessage2 = sha256(input.getBytes());
+        }
     }
     //function that will do sha 512 hashing
     public static String sha512(byte[] input){
@@ -22,7 +31,7 @@ public class Main {
         //Getting expanded message
         long[][] W = LogicSha512.Message(blocks);
 
-        //iNitializing bufer that will ultimately
+        //initializing bufer that will ultimately
         // store the hash message
         long[] buffer = Constants.initialBuffer.clone();
 
@@ -54,7 +63,7 @@ public class Main {
 
                 // Print out a-h
 
-                    System.out.printf("R%d abcdefgh: %016x %016x %016x %016x %016x %016x %016x %016x\n", j, a, b, c, d, e, f, g, h);
+                System.out.printf("R%d abcdefgh: %016x %016x %016x %016x %016x %016x %016x %016x\n", j, a, b, c, d, e, f, g, h);
             }
             // After finishing the compression, save the state to the buffer
             buffer[0] = a + buffer[0];
@@ -68,6 +77,7 @@ public class Main {
 
             System.out.printf("Hash for block %d: %016x %016x %016x %016x %016x %016x %016x %016x\n", i, buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7]);
         }
+
         // After everything is done, return the final hash as a string
         String result = "";
         for (int i = 0; i < 8; i++) {
@@ -76,5 +86,71 @@ public class Main {
 
         return result;
     }
+
+    //fUNCTION that will do the actual Sha256
+    public static String sha256(byte[] input){
+        //Padding
+        input = LogicSha256.padding(input);
+
+        //Generating Blocks
+        int [][] blocks = LogicSha256.convertToBlocks(input);
+
+        //Getting expanded message
+        int[][] W = LogicSha256.Message(blocks);
+
+        //initializing bufer that will ultimately
+        // store the hash message
+        int[] buffer = Constants.initialBuffer256.clone();
+
+        //For every blocks
+        for(int i=0;i<blocks.length;i++){
+            // a-h is set to the buffer initially
+            int a = buffer[0];
+            int b = buffer[1];
+            int c = buffer[2];
+            int d = buffer[3];
+            int e = buffer[4];
+            int f = buffer[5];
+            int g = buffer[6];
+            int h = buffer[7];
+
+            //perform round operation for 64 times in each block
+            //update the a to h buffer in each round
+            for (int j = 0; j < 64; j++) {
+                int t1 = h + LogicSha256.summation1To256(e) + LogicSha256.conditionFunction(e, f, g) + Constants.K256[j] + W[i][j];
+                int t2 = LogicSha256.summation0To256(a) + LogicSha256.majorityFunction(a, b, c);
+                h = g;
+                g = f;
+                f = e;
+                e = d + t1;
+                d = c;
+                c = b;
+                b = a;
+                a = t1 + t2;
+
+                // Print out a-h
+
+                System.out.printf("R%d abcdefgh: %08x %08x %08x %08x %08x %08x %08x %08x\n", j, a, b, c, d, e, f, g, h);
+            }
+            // After finishing the compression, save the state to the buffer
+            buffer[0] = a + buffer[0];
+            buffer[1] = b + buffer[1];
+            buffer[2] = c + buffer[2];
+            buffer[3] = d + buffer[3];
+            buffer[4] = e + buffer[4];
+            buffer[5] = f + buffer[5];
+            buffer[6] = g + buffer[6];
+            buffer[7] = h + buffer[7];
+
+            System.out.printf("Hash for block %d: %08x %08x %08x %08x %08x %08x %08x %08x\n", i, buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7]);
+        }
+        // After everything is done, return the final hash as a string
+        String result = "";
+        for (int i = 0; i < 8; i++) {
+            result += String.format("%08x", buffer[i]);
+        }
+
+        return result;
     }
+}
 
